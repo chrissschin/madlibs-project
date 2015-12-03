388cb444d1d27bbb2156686b5712c3f5d0f7648b
chrissschin <solischris92@gmail.com>
2015-12-02 18:46:55 -0800

    got gender change to work

--- myApp.js
+++ myApp.js
@@ -1,2 +1,18 @@
 angular.module("myApp",[])
-.controller("")
+.controller("myCtrl", function($scope){
+  $scope.femalename = "";
+  $scope.jobtitle = "";
+  $scope.tedioustask = "";
+  $scope.dirtytask = "";
+  $scope.celebrity = "";
+  $scope.uselessskill = "";
+  $scope.obnoxious = "";
+  $scope.hugenumber = "";
+  $scope.adjective = "";
+
+  $scope.gender = {
+      pronoun: 'she',
+      poss: 'her'
+  };
+
+});
