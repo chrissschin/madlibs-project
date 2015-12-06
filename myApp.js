a3be376db45aa612f0e1cf421738ccc6a149c07b
chrissschin <solischris92@gmail.com>
2015-12-05 17:46:57 -0800

    added show / hide

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 angular.module("myApp",[])
 .controller("myCtrl", function($scope){
+  var self = this;
   $scope.femalename = "";
   $scope.jobtitle = "";
   $scope.tedioustask = "";
@@ -14,5 +15,36 @@ angular.module("myApp",[])
       pronoun: 'she',
       poss: 'her'
   };
+  //sets output to hide first
+  $scope.madLibOut = false;
+
+
+  $scope.inputWords = true;
+
+  //shows output of main-content
+  $scope.genMadLib = function() {
+    this.madLibOut = true;
+    this.inputWords = false;
+  }
+
+  $scope.reset = function() {
+    this.madLibOut = false;
+    this.inputWords = true;
+    this.femalename = "";
+    this.jobtitle = "";
+    this.tedioustask = "";
+    this.dirtytask = "";
+    this.celebrity = "";
+    this.uselessskill = "";
+    this.obnoxious = "";
+    this.hugenumber = "";
+    this.adjective = "";
+
+    this.inputForm.$setPristine();
+
+  }
+
+
+  // $scope.mainCont = false;
 
 });
